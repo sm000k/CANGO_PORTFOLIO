45e5a61a4d34bd3712691c5a3abc2616d9cbf470
sm000k <smok.lacki@gmail.com>
2024-10-25 21:22:54 +0200

    TM-4-conversion from maven to gradle

---
+++ src/test/java/pl/cango/demo/DemoApplicationTests.java
@@ -0,0 +1,14 @@
+package pl.cango.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import pl.cango.demo.persistence.repository.CompanyRepository;
+
+@SpringBootTest
+class DemoApplicationTests {
+	private CompanyRepository companyRepository;
+	@Test
+	void contextLoads() {
+	}
+
+}
