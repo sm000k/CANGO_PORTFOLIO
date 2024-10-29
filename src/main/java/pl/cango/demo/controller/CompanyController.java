c2ac403b629d90a3f389f0f15cc8da2a3b36bbc6
sm000k <smok.lacki@gmail.com>
2024-10-29 21:15:40 +0100

    TM-8.1 Controllers poprawa Controllera Company

--- src/main/java/pl/cango/demo/controller/CompanyController.java
+++ src/main/java/pl/cango/demo/controller/CompanyController.java
@@ -1,11 +1,7 @@
 package pl.cango.demo.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import pl.cango.demo.model.Company;
 import pl.cango.demo.persistence.repository.CompanyRepository;
 
@@ -19,12 +15,20 @@ public class CompanyController {
     public CompanyController(CompanyRepository companyRepository) {
         this.companyRepository = companyRepository;
     }
-    @PostMapping ("/Company/save")
-    public Company createCompany (@RequestBody Company company) {
+
+    @PostMapping("/Company/save")
+    public Company createCompany(@RequestBody Company company) {
         return companyRepository.save(company);
     }
-    @GetMapping ("/Company/FindAll")
+
+    @GetMapping("/Company/")
     public List<Company> findAll() {
         return companyRepository.findAll();
     }
+
+    @GetMapping("/Company/{name}")
+    public List<Company> findByName(@PathVariable String name) {
+        return companyRepository.findCompaniesByNameContainingIgnoreCase(name);
+    }
+
 }
--- src/main/java/pl/cango/demo/persistence/repository/CompanyRepository.java
+++ src/main/java/pl/cango/demo/persistence/repository/CompanyRepository.java
@@ -4,9 +4,13 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import pl.cango.demo.model.Company;
 
+import java.util.List;
 import java.util.UUID;
 
 @Repository
 public interface CompanyRepository extends JpaRepository<Company, UUID> {
+
+    List<Company> findCompaniesByNameContainingIgnoreCase(String name);
+
 
 }
