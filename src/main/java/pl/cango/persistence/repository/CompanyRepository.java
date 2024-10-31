16ca2dfacefca25a29f2e213f09c493f5bf1a5b0
sm000k <smok.lacki@gmail.com>
2024-10-31 23:33:01 +0100

    TM-8 porządki zmiana z uuid na string

--- src/main/java/pl/cango/persistence/repository/CompanyRepository.java
+++ src/main/java/pl/cango/persistence/repository/CompanyRepository.java
@@ -5,10 +5,9 @@ import org.springframework.stereotype.Repository;
 import pl.cango.model.Company;
 
 import java.util.List;
-import java.util.UUID;
 
 @Repository
-public interface CompanyRepository extends JpaRepository<Company, UUID> {
+public interface CompanyRepository extends JpaRepository<Company, String> {
 
     List<Company> findByNameContainingIgnoreCase(String name);
 }
