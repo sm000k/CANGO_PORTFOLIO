739dd850fa04c6c479b66aad82e000a0f7d63be8
sm000k <smok.lacki@gmail.com>
2024-11-06 15:11:35 +0100

    Cango 18 poprawki

--- src/main/java/pl/cango/service/AliasService.java
+++ src/main/java/pl/cango/service/AliasService.java
@@ -1,8 +1,10 @@
 package pl.cango.service;
 
 
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 import pl.cango.dto.CreateAliasRequest;
+import pl.cango.dto.CreateAliasResponse;
 import pl.cango.model.Alias;
 import pl.cango.persistence.repository.AliasRepository;
 import pl.cango.persistence.repository.ServiceRepository;
@@ -10,18 +12,14 @@ import pl.cango.persistence.repository.ServiceRepository;
 import java.util.Random;
 import java.util.UUID;
 
+@AllArgsConstructor
 @Service
 public class AliasService {
 
     private final AliasRepository aliasRepository;
     private final ServiceRepository serviceRepository;
 
-    public AliasService(AliasRepository aliasRepository, ServiceRepository serviceRepository) {
-        this.aliasRepository = aliasRepository;
-        this.serviceRepository = serviceRepository;
-    }
-
-    public CreateAliasRequest createAlias(CreateAliasRequest request) {
+    public CreateAliasResponse createAlias(CreateAliasRequest request) {
         if (!serviceRepository.existsById(request.getServiceId())) {
             throw new IllegalArgumentException("Service with ID " + request.getServiceId() + " does not exist.");
         }
@@ -35,7 +33,7 @@ public class AliasService {
 
         aliasRepository.save(serviceAlias);
 
-        return CreateAliasRequest.builder()
+        return CreateAliasResponse.builder()
                 .name(request.getName())
                 .serviceId(request.getServiceId())
                 .build();
